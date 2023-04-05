911e228c336c6f20324b0c5878e00c5321eb55cc
ashik2765 <ashikurrahman2765@gmail.com>
2023-04-06 00:03:30 +0600

    add button on cart

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const {img,name,price,rating,seller,quantity}=props.product
+    const { img, name, price, ratings, seller, quantity } = props.product
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h3>{name}</h3>
-            <h4>Price: ${price}</h4>
-            <p>Quantity: {quantity}</p>
-            <p>Manufacturer: {seller}</p>
-            <button>Buy Now</button>
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p><b>Price: ${price}</b></p>
+                <p>Manufacturer: {seller}</p>
+                <p>Ratings: {ratings} Star</p>
+                
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
