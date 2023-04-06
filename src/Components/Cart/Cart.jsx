863a7899a36136bc59b0ac78f7bbcf9bd8a14440
ashik2765 <ashikurrahman2765@gmail.com>
2023-04-06 20:56:20 +0600

    add localStorage and get from localStorage

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -7,6 +7,7 @@ const Cart = ({cart}) => {
     let totalShipping = 0;
     let quantity = 0;
     for(const product of cart){
+        // product.quantity = product.quantity || 1;
         total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
         quantity = quantity + product.quantity;
@@ -21,8 +22,8 @@ const Cart = ({cart}) => {
             <p>Selected Items: {quantity}</p>
             <p>Total Price: BDT {total}</p>
             <p>Shipping: BDT {totalShipping}</p>
-            <p>Tax: BDT {tax}</p>
-            <h4> Grand Total: BDT {grandTotal}</h4>
+            <p>Tax: BDT {tax.toFixed(2)}</p>
+            <h4> Grand Total: BDT {grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
